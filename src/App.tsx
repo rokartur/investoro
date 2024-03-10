ab1990ee90ac32dd526ad63d8bb7d0df7795439c
rokartur <rokartur04@icloud.com>
2024-03-10 20:05:37 +0100

    chore: update dependencies
    Took 5 minutes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import './assets/styles/globals.scss'
 import { useScrollToTop } from './hooks/useScrollToTop.ts'
-import { Provider } from 'react-redux'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
-import { store } from './utils/store.ts'
 import { Suspense } from 'react'
 import { Header } from './components/header/header.tsx'
 import { Home } from './views/home.tsx'
@@ -18,22 +16,20 @@ export const App = () => {
 	useScrollToTop()
 
 	return (
-		<Provider store={store}>
-			<HelmetProvider>
-				<Suspense fallback={<p>loading...</p>}>
-					<BrowserRouter>
-						<Header />
+		<HelmetProvider>
+			<Suspense fallback={<p>loading...</p>}>
+				<BrowserRouter>
+					<Header />
 
-						<Routes>
-							<Route path={'/'} element={<Home />} />
-							<Route path={'/offer'}>
-								<Route index path={':id'} element={<Offer />} />
-							</Route>
-							<Route path={'*'} element={<NotFound />} />
-						</Routes>
-					</BrowserRouter>
-				</Suspense>
-			</HelmetProvider>
-		</Provider>
+					<Routes>
+						<Route path={'/'} element={<Home />} />
+						<Route path={'/offer'}>
+							<Route index path={':id'} element={<Offer />} />
+						</Route>
+						<Route path={'*'} element={<NotFound />} />
+					</Routes>
+				</BrowserRouter>
+			</Suspense>
+		</HelmetProvider>
 	)
 }
